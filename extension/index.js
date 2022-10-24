396c53bf2df3e2f9d289c7c042ae34b21de5197c
J-P Tuomanen <jukka-pekka.tuomanen@metropolia.fi>
2022-10-24 03:51:20 +0300

    Toimii

---
+++ extension/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function (nodecg) {
+	nodecg.Replicant("speakerReplicant", { defaultValue: "Petteri Punakuono" });
+	nodecg.Replicant("titleReplicant", {defaultValue: "title"});
+};
